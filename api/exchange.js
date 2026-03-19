3d23d933fa31f474428e3d9e710f138f746b1501
PrithvirajuVenkataraman <147894867+PrithvirajuVenkataraman@users.noreply.github.com>
2026-03-20 04:09:42 +0530

    Update exchange.js

--- api/exchange.js
+++ api/exchange.js
@@ -1,5 +1,4 @@
 import { runVerifiedWebSearch } from './live-search.js';
-
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
