d6d3135cef82b6a53c4d8b1bb8d168206eb70f5b
PrithvirajuVenkataraman <147894867+PrithvirajuVenkataraman@users.noreply.github.com>
2026-03-20 04:11:52 +0530

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,5 +1,6 @@
 import { extractSearchTopic, runVerifiedWebSearch, searchWeb } from './live-search.js';
 
+
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
